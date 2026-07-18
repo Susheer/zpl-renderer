3f206ca473e6d2d8aaa3fb4d5d320511778a7fdb
spgupta <sgupta@bydefense.com>
2026-07-18 14:34:55 +0530

    Project exports Added

---
+++ managed/ZplRenderer.Core/Exports.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace ZplRenderer.Core;
+
+public static class Exports
+{
+    [UnmanagedCallersOnly(EntryPoint = "Initialize")]
+    public static int Initialize()
+    {
+        return 1;
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "GetVersion")]
+    public static IntPtr GetVersion()
+    {
+        return Marshal.StringToCoTaskMemUTF8("0.1.0");
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "FreeMemory")]
+    public static void FreeMemory(IntPtr ptr)
+    {
+        if (ptr != IntPtr.Zero)
+        {
+            Marshal.FreeCoTaskMem(ptr);
+        }
+    }
+}
